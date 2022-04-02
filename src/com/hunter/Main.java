b670afda83b0c04b576f466f3b15684eea92343f
Yiğit Avcı <yigit.avci@solviads.com>
2022-04-02 11:41:35 +0300

    initial commit

---
+++ src/com/hunter/Main.java
@@ -0,0 +1,8 @@
+package com.hunter;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("this is a project to test \"git reset --soft HEAD~1\"");
+    }
+}
